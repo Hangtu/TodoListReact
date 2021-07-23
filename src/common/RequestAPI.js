3ba7da523441467c721ff405b16886e916e0780c
Hang Tu Wong Ley Franco <hangtuwlf@gmail.com>
2021-07-22 19:14:47 -0500

    just adding axios defaults

--- src/common/RequestAPI.js
+++ src/common/RequestAPI.js
@@ -1,30 +1,24 @@
 import axios from "axios";
-const headers = {
-  "X-Api-Key":
-    "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c",
-};
+
+axios.defaults.baseURL = 'HTTPS://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io';
+axios.defaults.headers.common['X-Api-Key'] = 'PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c';
+axios.defaults.headers.patch['Content-Type'] = 'application/json';
 
 export async function getListItems() {
   const response = axios({
-    url: "https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/get",
+    url: "/get",
     method: "get",
-    headers: headers,
   });
-
   return response;
 }
 
 export async function updateItemRequest(item, payload) {
-  headers["Content-Type"] = "application/json";
-
   const response = axios({
-    url: `https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/${item.id}`,
+    url: `/patch/${item.id}`,
     method: "patch",
-    headers: headers,
     data: {
       isComplete: payload,
     },
   });
-
   return response;
 }
